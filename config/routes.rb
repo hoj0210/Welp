dcd66435ea33480edd3f270f7bc6bab01cb5ed41
Ho Jung Cha <hojungcha@Hos-MacBook-Pro.lan1>
2021-01-19 13:59:17 -0800

    started working on the routes for the usercontroller and sessionscontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'users/api/sessions'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#root'
+  namespace :api, defaults: {format: json} do 
+    resource :user, only: [:create]
+  end
 end
