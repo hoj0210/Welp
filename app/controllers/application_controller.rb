e2e8e94e445bfe7348925501e2dd0aa8794bdb77
Ho Jung Cha <hojungcha@Hos-MacBook-Pro.lan1>
2021-01-19 12:48:07 -0800

    migrated user table and craeted user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :ensure_login, :logged_in?
+
+    private
+
+    def current_user 
+        return nil unless session[:session_token]
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def ensure_login 
+        redirect_to
+
 end
---
+++ app/models/user.rb
@@ -0,0 +1,41 @@
+class User < ApplicationRecord 
+    validates :fname, :lname, :email, :zip_code, :password_digest, :session_token, presence: true
+    validates :email, uniqueness: true
+    validates :password, length: { minimum: 8 }, allow_nil: true
+
+    has_many :reviews,
+        primary_key: :id,
+        foreign_key: :user_id
+        class_name: :Review 
+
+    attr_reader :password 
+    after_initialize :ensure_session_token 
+
+    def self.find_by_credentials(email, password) 
+        user = User.find_by(email: email)
+        return user if user && user.is_password?(password)
+    end
+
+    def is_password?(passwrd)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def password=(password)
+        @password = password 
+        self.password_digest = BCrypt::Password.create(password)
+    end
+    
+    def reset_session_token!
+        self.update!(session_token: self.class.generate_session_token)
+        self.session_token
+
+    private 
+
+    def ensure_session_token 
+        self.session_token ||= self.class.generate_session_token
+    end
+
+    def self.generate_session_token
+        SecrureRandom::urlsafe_base64(16)
+    end
+end
