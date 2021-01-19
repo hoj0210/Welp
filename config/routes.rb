8cb3dd35be4522eeb04b36f6e9f582cc92d02dd6
Ho Jung Cha <hojungcha@Hos-MacBook-Pro.lan1>
2021-01-19 14:32:02 -0800

    Prepare for initial Heroku push

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#root'
-  namespace :api, defaults: {format: json} do 
+  namespace :api, defaults: {format: JSON} do 
     resource :user, only: [:create]
   end
 end
