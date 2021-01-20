76934438016e28a16912a19ca341a7873c2d0c88
Ho Jung Cha <hojungcha@Hos-MacBook-Pro.attlocal.net>
2021-01-19 22:33:09 -0800

    added window for testing for state

--- frontend/welp.jsx
+++ frontend/welp.jsx
@@ -4,7 +4,10 @@ import configureStore from './store/store'
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById('root');
-
+    //testing
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
     const store = configureStore();
     ReactDOM.render(<h1>hello</h1>, root)
+
 })
