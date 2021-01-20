921ca3063479156ab635b47e0800e628486df7cb
Ho Jung Cha <hojungcha@Hos-MacBook-Pro.local>
2021-01-19 17:36:23 -0800

    finished session_api_util

--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -1,5 +1,22 @@
 export const signup = (user) => (
-    +{
-        
+    {
+        url: '/api/user',
+        method: 'POST',
+        data: { user }
+    }
+)
+
+export const login = (user) => (
+    {
+        url: '/api/session',
+        method: 'POST',
+        data: { user }
+    }
+)
+
+export const logout = () => (
+    {
+        url: '/api/session',
+        method: 'DELETE'
     }
 )
