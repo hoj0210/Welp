d457b0ffab51b71da7a9852a1e9f4c6330a9812a
Ho Jung Cha <hojungcha@Hos-MacBook-Pro.lan1>
2021-01-22 12:20:13 -0800

    fixed syntax extra validation?? keep getting error

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,15 +1,15 @@
 class Business < ApplicationRecord
-    validates 
-        :name,
-        :address,
-        :city,
-        :state,
-        :b_zip_code,
-        :open,
-        :close,
-        :phone_num,
-        :price_range,
-        :longitude,
-        :latitude,
-        presence: true 
+    # validates 
+    #     :name,
+    #     :address,
+    #     :city,
+    #     :state,
+    #     :b_zip_code,
+    #     :open,
+    #     :close,
+    #     :phone_num,
+    #     :price_range,
+    #     :longitude,
+    #     :latitude,
+    #     presence: true 
 end
