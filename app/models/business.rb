4a332aaf9d06bd89a12d262434e9b136726bdeaf
Ho Jung Cha <hojungcha@Hos-MacBook-Pro.lan1>
2021-01-22 00:03:26 -0800

    added first validations to business model

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,3 +1,16 @@
 class Business < ApplicationRecord
     validates 
+        :name,
+        :address,
+        :city,
+        :state,
+        :b_zip_code,
+        :open,
+        :close,
+        :phone_num,
+        :price_range,
+        :longitude,
+        :latitude,
+        presence: true 
+    validates 
 end
