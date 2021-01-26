7eb1afbb6cddc2fe7c17f5ebdc4e5c5db71ccfbe
Ho Jung Cha <hojungcha@Hos-MacBook-Pro.local>
2021-01-26 13:25:52 -0800

    finished about footer

--- frontend/components/about/about.jsx
+++ frontend/components/about/about.jsx
@@ -2,25 +2,64 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const About = props => (
-    <div className="about-footer">
-        <p className="about-title">About</p>
-        <div className="about-links">
-            <div className="about-link">
-                <a href="https://github.com/hoj0210">Github</a>
+    <div className="footer-container">
+        <div className="footer-stuff">
+            <div className="footer-about">
+                <p className="footer-title">About</p>
+                <div className="about-links">
+                    <div className="about-link">
+                        <a href="https://github.com/hoj0210">Github</a>
+                    </div>
+                    <div className="about-link">
+                        <a href="https://www.facebook.com/hoj0210">Facebook</a>
+                    </div>
+                    <div className="about-link">
+                        <a href="">LinkedIn</a>
+                    </div>
+                    <div className="about-link">
+                        <a href="">Instagram</a>
+                    </div>
+                </div>  
             </div>
-            <div className="about-link">
-                <a href="https://www.facebook.com/hoj0210">Facebook</a>
+            <div className="footer-about">
+                <p className="footer-title">Projects</p>
+                <div className="about-links">
+                    <div className="about-link">
+                        <p>AA-Welp</p>
+                    </div>
+                    <div className="about-link">
+                        <p>Widgets</p>
+                    </div>
+                    <div className="about-link">
+                        <p>Knights Travail</p>
+                    </div>
+                    <div className="about-link">
+                        <p>Tic Tac Toe</p>
+                    </div>
+                </div>
             </div>
-            <div className="about-link">
-                <a href="">LinkedIn</a>
-            </div>
-            <div className="about-link">
-                <a href="">Instagram</a>
-            </div>
-            <div className="bottom-image">
-                <img src={window.bottomImg} alt=""/>
+            <div className="footer-about">
+                <p className="footer-title">Relax</p>
+                <div className="about-links">
+                    <div className="about-link">
+                        <p>Friends</p>
+                    </div>
+                    <div className="about-link">
+                        <p>Harry Potter</p>
+                    </div>
+                    <div className="about-link">
+                        <p>Cello</p>
+                    </div>
+                    <div className="about-link">
+                        <p>IMDB</p>
+                    </div>
+                </div>
             </div>
         </div>
+        <img className="bottom-image" src={window.bottomImg} alt=""/>
+        {/* <div className="bottom-image">
+            <img src={window.bottomImg} alt=""/>
+        </div> */}
     </div>
 )
 
