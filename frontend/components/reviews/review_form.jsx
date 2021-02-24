ad4de21d47f56668cfbe9e6ccb25bc734e5e4f42
Hojung Cha <hoj0210@gmail.com>
2021-02-24 11:41:31 -0800

    star review implemented in review rating

--- frontend/components/reviews/review_form.jsx
+++ frontend/components/reviews/review_form.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import ReviewNavBarContainer from '../nav_bar/review_nav_bar_container'
+import Rating from 'react-rating';
 
 class ReviewForm extends React.Component {
     constructor(props){
@@ -11,9 +12,13 @@ class ReviewForm extends React.Component {
             user_id: this.props.user_id,
             business_id: this.props.match.params.businessId
         }
-        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleRatingChange = this.handleRatingChange.bind(this);
     }
-
+    handleRatingChange(n){
+        debugger
+        this.setState({rating: n})
+    }
     handleChange(type){
         return(e) => (
             this.setState({[type]: e.currentTarget.value})
@@ -26,6 +31,7 @@ class ReviewForm extends React.Component {
     }
 
     handleSubmit(e){
+        debugger
         e.preventDefault(); 
         this.props.createReview(this.state).then(()=> this.props.history.push(`/businesses/${this.state.business_id}`))
     }
@@ -53,7 +59,13 @@ class ReviewForm extends React.Component {
                                 </div>
                                 <div className="review-content-box">
                                     <p className="select-rating-text">Select your rating</p>
-                                    <input type="number" min="1" max="5" className="rating-number"value={this.state.rating} onChange={this.handleChange("rating")}/>
+                                    <Rating
+                                        emptySymbol="fa fa-star-o fa-2x"
+                                        initialRating={this.state.rating}
+                                        fullSymbol="fa fa-star fa-2x"
+                                        onChange={this.handleRatingChange}
+                                    />
+                                    {/* <input type="number" min="1" max="5" className="rating-number"value={this.state.rating} onChange={this.handleChange("rating")}/> */}
                                     <textarea className="text-area-text"value={this.state.message} onChange={this.handleChange("message")} 
                                     placeholder="It's amazing that they've added delivery due to COVID. The delivery wasn't perfert--they forgot one of my side dishes--but I understand this is a new operation for them at this time. Even so, the burrito was delicious and more than made up for it!"></textarea>
                                 </div>
