310d260d916eee1241ceb47536e8b07e1368a9b5
Hojung Cha <hoj0210@gmail.com>
2021-02-24 22:38:33 -0800

    changed splash page to have popular searches

--- frontend/components/popular_searches/popular_searches.jsx
+++ frontend/components/popular_searches/popular_searches.jsx
@@ -3,31 +3,31 @@ import { Link } from 'react-router-dom'
 
 const PopularSearches = props => (
     <div>
-        <h1 className="splash-box-title">Find the Best Businesses in Town</h1>
+        <h1 className="splash-box-title">Find the Best Restaurants by Cuisine</h1>
         <div className="popular-search-container">
             <div className="popular-search-item">
                 <div className="popular-search-pic-c">
                     <img className="popular-search-pic" src={window.tacos} alt=""/>
                 </div>
-                <p className="popular-search-text">Tacos</p>
+                <p className="popular-search-text">Mexican</p>
             </div>
             <div className="popular-search-item">
                 <div className="popular-search-pic-c">
-                    <img className="popular-search-pic" src={window.loungeBar}/>
+                    <img className="popular-search-pic" src={window.koreanCuisine}/>
                 </div>
-                <p className="popular-search-text">Lounge Bar</p>  
+                <p className="popular-search-text">Korean</p>  
             </div>
             <div className="popular-search-item">
                 <div className="popular-search-pic-c">
-                    <img className="popular-search-pic" src={window.auto}/>
+                    <img className="popular-search-pic" src={window.italianCuisine}/>
                 </div>
-                <p className="popular-search-text">Auto-Repair</p>
+                <p className="popular-search-text">Italian</p>
             </div>
             <div className="popular-search-item">
                 <div className="popular-search-pic-c">
-                    <img className="popular-search-pic" src={window.shopping}/>
+                    <img className="popular-search-pic" src={window.americanCuisine}/>
                 </div>
-                <p className="popular-search-text">Malls</p>
+                <p className="popular-search-text">American</p>
             </div>
         </div>
     </div>
