8ced7cfd7e8ab3fe5494aa96491a27db4991a0a0
Hojung Cha <hoj0210@gmail.com>
2021-03-03 13:24:29 -0800

    renders the search

--- frontend/components/search_box/search_box.jsx
+++ frontend/components/search_box/search_box.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, withRouter} from 'react-router-dom';
+import {connect} from 'react-redux'
 
 class SearchBox extends React.Component {
     constructor(props){
@@ -8,8 +9,9 @@ class SearchBox extends React.Component {
         this.find = (this.props.formType === "splash") ? "Find" : ""
         this.near = (this.props.formType === "splash") ? "Near" : ""
         this.state = {
-
+            query: ""
         }
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
     
     handleChange(type){
@@ -19,18 +21,20 @@ class SearchBox extends React.Component {
     }
 
     handleSubmit(e){
+        debugger
         e.preventDefault()
+        this.props.searchBusinesses(this.state.query).then(() => this.props.history.push(`/businesses?query=${this.state.query}`) )
     }
 
     render(){
         return(
             <div className={`search-box-container-${this.type}`}>
                 <div className={`search-box${this.type}`}>
-                    <form>
+                    <form onSubmit={this.handleSubmit}>
                         <div className={`search-texts${this.type}`}>
                             <label className={`search-word1${this.type}`}>
                                 {this.find}
-                                <input className={`search-text${this.type}`}type="text" placeholder="tacos, Japanese, Phil's BBQ..." />
+                                <input className={`search-text${this.type}`}type="text" placeholder="tacos, Japanese, Phil's BBQ..." onChange={this.handleChange("query")} />
                             </label>
                             <label className={`search-word2${this.type}`}>{this.near}
                                 <input className={`search-text${this.type}`}type="text" placeholder="New York and Los Angeles ONLY FOR NOW" />
@@ -47,4 +51,15 @@ class SearchBox extends React.Component {
     }
 }
 
-export default SearchBox
+const msp = (state, ownProps) => {
+    return {
+
+    }
+}
+
+const mdp = dispatch => (
+    {
+
+    }
+)
+export default withRouter(connect(msp, mdp)(SearchBox))
