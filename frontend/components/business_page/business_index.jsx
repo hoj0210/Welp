478bb0e919c62f6b81eb5ec630a1a914b0ec2c53
Hojung Cha <hoj0210@gmail.com>
2021-03-04 07:47:28 -0800

    numbered the index, style the font

--- frontend/components/business_page/business_index.jsx
+++ frontend/components/business_page/business_index.jsx
@@ -54,15 +54,17 @@ class BusinessIndex extends React.Component{
                         <p className="best-in">Best {this.state.search} in {}</p>
                         <p className="all-results">All Results</p>
                         <div className="results">
-                            {this.props.businesses.map(business => {
+                            {this.props.businesses.map((business, i) => {
                                 debugger
                                 return(
-                                    <div className="each-result-container">
-                                        <Link to={`/businesses/${business.id}`}>
+                                    <div className="each-result-container" key={i}>
+                                        <Link className="result-link" to={`/businesses/${business.id}`}>
                                             <div className="each-result">
                                                 <img className="b_index_pic"src={window.stockPhoto} alt=""/>
                                                 {/* <img className="b_index_pic"src={business.photoUrls[0]} alt=""/> */}
-                                                {business.name}
+                                                <div className="result-info">
+                                                    <p className="result-name">{i + 1}. {business.name}</p>
+                                                </div>
                                             </div>
                                         </Link>
                                         
